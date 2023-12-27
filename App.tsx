4a71b4ab85479c347745ee2d3861a3f5ed742efa
archsaint <hieu.truong.0214@gmail.com>
2023-12-27 22:53:52 +0700

    setup location permission

--- App.tsx
+++ App.tsx
@@ -6,38 +6,64 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
+
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
   useColorScheme,
   View,
 } from 'react-native';
 
 import {
   Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+
 import {enableLatestRenderer} from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
+
+import {check, request, PERMISSIONS, RESULTS} from 'react-native-permissions';
+
+
+request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION).then((result) => {
+  console.log({result})
+});
+
+check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION)
+  .then((result) => {
+    switch (result) {
+      case RESULTS.UNAVAILABLE:
+        console.log('This feature is not available (on this device / in this context)');
+        break;
+      case RESULTS.DENIED:
+        console.log('The permission has not been requested / is denied but requestable');
+        break;
+      case RESULTS.LIMITED:
+        console.log('The permission is limited: some actions are possible');
+        break;
+      case RESULTS.GRANTED:
+        console.log('The permission is granted');
+        break;
+      case RESULTS.BLOCKED:
+        console.log('The permission is denied and not requestable anymore');
+        break;
+    }
+  })
+  .catch((error) => {
+    console.log({error})
+  });
 
 enableLatestRenderer().then(console.log);
-import MapView, {Marker} from 'react-native-maps';
+
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    flex: 1,
   };
 
   return (
-    <View style={{flex: 1, backgroundColor: 'powderblue'}}>
+    <View style={backgroundStyle}>
        <MapView
          initialRegion={{
            latitude: 37.78825,
